58efde92233845624524d3ffb492f4fdad11fb0c
Marc Ford <mrfxyz567@gmail.com>
2020-08-06 16:30:11 -0400

    Add frontend dist

---
+++ beaconator/frontend/dist/precache-manifest.35131872043c924a0bff746743fcab14.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8949274694c4bd514ca9",
+    "url": "/admin/css/app.6d25fe13.css"
+  },
+  {
+    "revision": "43efc88da286d263bdb9",
+    "url": "/admin/css/codes~properties.1925903d.css"
+  },
+  {
+    "revision": "c8d225679a51c06b01367e8b3a8bb27c",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "8949274694c4bd514ca9",
+    "url": "/admin/js/app.24d823d1.js"
+  },
+  {
+    "revision": "57c146603c2b6bc53622",
+    "url": "/admin/js/chunk-vendors.39778d2d.js"
+  },
+  {
+    "revision": "1187b8ae1f473e7ea1a1",
+    "url": "/admin/js/codes.4b46a7a5.js"
+  },
+  {
+    "revision": "43efc88da286d263bdb9",
+    "url": "/admin/js/codes~properties.05912c4a.js"
+  },
+  {
+    "revision": "43d4948ed6c671a9574c",
+    "url": "/admin/js/properties.ef389976.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin/robots.txt"
+  }
+]);
